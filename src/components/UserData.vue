8df0bb0d0081f56cb053f288e5395effd8b6b864
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-01-20 15:00:10 -0400

    Segundo parametro en setup Custom Events
    - El segundo componente es context para ser usado con attrs y emit
    - Emit se usa de la siguiente manera: ej. context.emit(...).

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -12,10 +12,15 @@ export default {
   //         return this.firstName + ' ' + this.lastName;
   //     }
   // }
-  setup(props) {
+  setup(props, context) {
     const uName = computed(() => {
       return props.firstName + ' ' + props.lastName;
     });
+
+    console.log(context);
+
+    // Forma de usar emit en Composition API, por medio de context
+    // context.emit('save-data', 1); // this.$emit('save-data', 1);
     return { userName: uName };
   },
 };
